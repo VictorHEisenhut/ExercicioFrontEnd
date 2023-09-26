0fb98f6a9807588a09f0deb1003998e82d278155
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2023-09-26 17:02:50 -0300

    Realizando o exercício de listagem de arrays

--- ListagemDeArrays/scripts/script.js
+++ ListagemDeArrays/scripts/script.js
@@ -1,6 +1,26 @@
 let info = [];
+const aux = []
 
-function ordenar(){
+function ordenarDecrescente(){
+
+    for (let i = 0; i < info.length; i++) {
+
+        for (let j = 0; j < (info.length - i - 1); j++) {
+
+            if(info[j] < info[j + 1]){
+                var temp = info[j]
+                info[j] = info[j + 1]
+                info[j + 1] = temp;
+            }
+        }
+        
+    }
+
+    listarArray();
+
+}
+
+function ordenarCrescente(){
     
     for (let i = 0; i < info.length; i++) {
 
@@ -40,6 +60,55 @@ function listarArray() {
 
         i++
     }
-    console.log(itens)
+    aux.push(info)
     document.getElementsByTagName("ul")[0].innerHTML = itens
+}
+
+function pesquisar(input){
+
+    let num = parseFloat(document.getElementById(`${input}`).value);
+
+    if(info.includes(num)){
+        return info.indexOf(num);
+    }
+    else{
+        return -1;
+    }
+
+}
+
+function showIndex(){
+
+    let input = document.getElementById("input");
+
+    let index = pesquisar("inputNumero")
+
+    input.value = index;
+
+}
+
+function excluir(){
+
+    let index = pesquisar("inputExcluir");
+
+    info.splice(index, 1);
+
+    listarArray();
+}
+
+function editar(){
+
+    let indexAntigo = pesquisar("inputAntigo");
+    let indexNovo = document.getElementById("inputNovo");
+
+    if(pesquisar("inputNovo") == -1){
+        info[indexAntigo] = indexNovo.value;
+        listarArray();
+    }
+    else{
+        return;
+    }
+    
+
+
 }
