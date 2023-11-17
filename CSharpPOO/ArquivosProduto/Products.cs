4fc892d42c9ac567eee8d041a7f0b3ffd7c60e93
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2023-11-17 15:26:41 -0300

    Projeto para escrever e ler produtos em um arquivo

---
+++ CSharpPOO/ArquivosProduto/Products.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivosProduto
+{
+    internal class Products
+    {
+        private int codigo;
+        private string descricao;
+        private int estoque;
+        private double valorUnit;
+
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Código inválido");
+                    throw new FormatException();
+                }
+                else
+                {
+                    codigo = value;
+                }
+            }
+        }
+        public string Descricao
+        {
+            get
+            {
+                return descricao;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Descrição inválida");
+                    throw new FormatException();
+
+                }
+                else
+                {
+                    descricao = value;
+                }
+            }
+        }
+        public int Estoque
+        {
+            get
+            {
+                return estoque;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Estoque inválido!");
+                    throw new FormatException();
+
+                }
+                else
+                {
+                    estoque = value;
+                }
+            }
+        }
+        public double ValorUnit
+        {
+            get
+            {
+                return valorUnit;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Cor inválida!");
+                    throw new FormatException();
+
+                }
+                else
+                {
+                    valorUnit = value;
+                }
+            }
+        }
+
+        public Products()
+        {
+
+        }
+
+        public Products(int codigo, string descricao, int estoque, double valorUnit)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Estoque = estoque;
+            ValorUnit = valorUnit;
+        }
+
+        static string path = @"C:\Users\victor.eisenhut\Projects\CSharpPOO\Products.txt";
+
+        public static void CreateProduct(List<Products> products)
+        {
+            CreateFile();
+            try
+            {
+                Console.WriteLine("Digite o código do produto");
+                var codigo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite a descrição do produto");
+                var descricao = Console.ReadLine();
+
+                Console.WriteLine("Digite o estoque do produto");
+                var estoque = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor unitário do produto");
+                var valor = Convert.ToDouble(Console.ReadLine());
+
+                for (int i = 0; i < products.Count; i++)
+                {
+                    if (codigo == products[i].Codigo)
+                    {
+                        Console.WriteLine("Código inválida");
+                        throw new FormatException();
+                    }
+                }
+                Products product = new Products() { Codigo = codigo, Descricao = descricao, Estoque = estoque, ValorUnit = valor };
+                products.Add(product);
+                WriteInFile(products);
+
+                Console.Clear();
+                ReadProducts(products);
+
+            }
+            catch (FormatException)
+            {
+            }
+
+        }
+
+        public static void CreateFile()
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"Produtos:\n");
+
+                }
+            }
+        }
+        public static void WriteInFile(List<Products> products)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                foreach (var produto in products)
+                {
+                    sw.WriteLine(produto);
+                }
+
+            }
+
+        }
+        public static void ReadInFile()
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+     
+
+        public static void ReadProducts(List<Products> products)
+        {
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void UpdateProducts(List<Products> products)
+        {
+            //ReadPlacas(cars);
+            Console.WriteLine("Digite o código do produto a ser editado:");
+            var cod = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                for (int i = 0; i < products.Count; i++)
+                {
+                    if (products[i].Codigo.Equals(cod))
+                    {
+
+                        Console.WriteLine("Digite a descrição do produto");
+                        var desc = Console.ReadLine();
+
+                        Console.WriteLine("Digite o estoque do produto");
+                        var estoque = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Digite o valor do produto");
+                        var valor = Convert.ToDouble(Console.ReadLine());
+
+                        products[i] = new Products() { Codigo = products[i].Codigo, Descricao = desc, Estoque = estoque, ValorUnit = valor };
+                    }
+                }
+
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        public static void DeleteProducts(List<Products> products)
+        {
+            //ReadPlacas(cars);
+            Console.WriteLine("Digite o código do produto a ser excluído:");
+            var cod = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Codigo.Equals(cod))
+                {
+                    products.Remove(products[i]);
+                    Console.WriteLine("Produto removido!");
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"\nInformações do produto:\n" +
+                $"Codigo: {this.Codigo}\n" +
+                $"Descrição: {this.Descricao}\n" +
+                $"Estoque: {this.Estoque}\n" +
+                $"Valor unitário: {this.ValorUnit}";
+        }
+
+    }
+}
+
---
+++ CSharpPOO/ArquivosProduto/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ArquivosProduto
+{
+    internal class Program
+    {
+        public static List<Products> products = new();
+        static void Main(string[] args)
+        {
+            string condicao = "";
+
+            while (condicao != "SIM")
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "------MENU------\n" +
+                    "|[1] Adicionar    |\n" +
+                    "|[2] Ler produtos |\n" +
+                    "|[3] Ler arquivo  |\n" +
+                    "|[4] Editar       |\n" +
+                    "|[5] Remover      |");
+
+                try
+                {
+                    var opcao = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            Products.CreateProduct(products);
+                            break;
+                        case 2:
+                            Products.ReadProducts(products);
+                            break;
+                        case 3:
+                            Products.ReadInFile();
+                            break;
+                        case 4:
+                            Products.UpdateProducts(products);
+                            break;
+                        case 5:
+                            Products.DeleteProducts(products);
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+                Console.WriteLine("Deseja encerrar o programa? (sim) (pressione qualquer tecla)");
+                condicao = Console.ReadLine().ToUpper();
+
+            }
+        }
+    }
+}
