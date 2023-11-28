7d5995bb72a74cdf29c4689d12af2f936925a065
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2023-11-28 17:19:30 -0300

    Primeiro projeto mvc de contatos

---
+++ Asp.Net/ProjetoMVC/Controllers/ContatoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private List<Contato> contatos = new()
+        {
+            new Contato { Id = 1, Nome = "jamal", Email = "jamal@gmail.com" },
+            new Contato { Id = 2, Nome = "kleber", Email = "kleber@gmail.com" },
+            new Contato { Id = 3, Nome = "terance", Email = "terance@gmail.com" }
+        };
+        public IActionResult Index()
+        {
+            return View(contatos);
+        }
+
+        public IActionResult GetFirstContato()
+        {
+            return View(contatos.FirstOrDefault());
+        }
+    }
+}
