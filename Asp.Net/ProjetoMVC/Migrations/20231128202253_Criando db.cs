22085e98b5ab8ec1dc6ef08c327f22e9bd4b1c46
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2023-11-28 17:26:25 -0300

    Adicionando controller de produtos com crud e com acesso ao banco de dados via EntityFramework

---
+++ Asp.Net/ProjetoMVC/Migrations/20231128202253_Criando db.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoMVC.Migrations
+{
+    public partial class Criandodb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ Asp.Net/ProjetoMVC/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
