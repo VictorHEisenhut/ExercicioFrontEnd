8c348c72ffb80f46bd01d02059a1fa6c78203590
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2024-01-16 17:23:25 -0300

    Criando API de Categorias e produtos com CRUD completo

---
+++ API/CategoriaAPI/CategoriaAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CategoriaAPI.Data;
+using CategoriaAPI.Entities.Dtos;
+using AutoMapper;
+
+namespace CategoriaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly CategoriaContext _context;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(CategoriaContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            var prods = await _context.Produtos.ToListAsync();
+            foreach (var produto in prods)
+            {
+                produto.Categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.Id == produto.CategoriaId);
+            }
+            return prods;
+        }
+
+        // GET: api/Produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            produto.Categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.Id == produto.CategoriaId);
+
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+        
+        [HttpGet("{CategoriaId}")]
+        public async Task<ActionResult<List<Produto>>> GetProdutosByCategoria(int CategoriaId)
+        {
+            List<Produto> prods = new();
+            var produtos = await _context.Produtos.ToListAsync();
+            foreach (var produto in produtos)
+            {
+                if (produto.CategoriaId == CategoriaId)
+                {
+                    prods.Add(produto);
+                }
+            }   
+
+
+            if (produtos == null)
+            {
+                return NotFound();
+            }
+
+            return prods;
+        }
+
+        // PUT: api/Produtos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduto(int id, UpdateProdutoDto produtoDto)
+        {
+            var produto = _mapper.Map<Produto>(produtoDto);
+            produto.Id = id;
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produtos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(CreateProdutoDto produtoDto)
+        {
+            var produto = _mapper.Map<Produto>(produtoDto);
+            produto.Categoria = _context.Categorias.FirstOrDefault(c => c.Id == produtoDto.CategoriaId);
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", new { id = produto.Id }, produto);
+        }
+
+        // DELETE: api/Produtos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.Produtos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ API/CategoriaAPI/CategoriaAPI/Data/CategoriaContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Data.Common;
+
+namespace CategoriaAPI.Data
+{
+    public class CategoriaContext : DbContext
+    {
+        public CategoriaContext(DbContextOptions<CategoriaContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\victor.eisenhut\Documents\categoriaDb.mdf;Integrated Security=True;Connect Timeout=30";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
