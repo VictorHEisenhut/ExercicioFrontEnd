a924f8ddda28100e719a0ab926168e68295e882b
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2024-01-22 17:19:37 -0300

    Adicionando ID ao token

--- API/AuthenticationAPI/Config/TokenService.cs
+++ API/AuthenticationAPI/Config/TokenService.cs
@@ -18,7 +18,8 @@ namespace AuthenticationAPI.Config
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.Name, user.Username.ToString()),
-                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddHours(2),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
