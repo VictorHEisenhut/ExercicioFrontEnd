045a88d3a1ecae326a7dd92045f78700bbae9ec8
Victor Henrique Eisenhut <victor.eisenhut@proway.treina>
2024-01-22 16:27:37 -0300

    Criando autenticação e geração de token para api

---
+++ API/AuthenticationAPI/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AuthenticationAPI.Data;
+using AuthenticationAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using AuthenticationAPI.Config;
+using AuthenticationAPI.Dtos;
+
+namespace AuthenticationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+   
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<dynamic>> Login(UserDto user)
+        {
+            string token = "";
+            var users = await _context.Users.ToListAsync();
+            var userLogado = (from u in users where u.Username == user.Username & u.Password == user.Password select u).ToList();
+                //users.Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefault();
+
+            if (userLogado.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
+
+            token = TokenService.GenerateToken(userLogado[0]);
+
+            return new { token = token };
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ API/AuthenticationAPI/Data/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthenticationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAPI.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\victor.eisenhut\Documents\userDb.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+    }
+}
